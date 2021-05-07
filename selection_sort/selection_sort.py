538bf9e7dd3df2367048492a660e980d0d1dda68
Bryce Eaton <bryce.eaton86@gmail.com>
2021-05-07 14:30:43 -0500

    removed len - 1 since range is exclusive

--- selection_sort/selection_sort.py
+++ selection_sort/selection_sort.py
@@ -2,9 +2,11 @@ arr = [9,6,7,1,5,3,2,0,4,8]
 
 def selection_sort(arr):
     
-    for i in range(len(arr)-1):
+    for i in range(len(arr)):
+        # isolates the first value to compare to the rest of the list
         min_index = i
-        for j in range(i, len(arr)-1):
+        for j in range(i, len(arr)):
+            # find the lowest number in the rest of the list
             if arr[min_index] > arr[j]:
                 min_index = j
         arr[i], arr[min_index] = arr[min_index], arr[i]
